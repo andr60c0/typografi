ccbd1c030930dc72c92f604983da8d15531ea035
Andrea Valgeirsdóttir <andreavalgeirsdottir@with-ease.kea.local>
2020-02-20 13:44:05 +0100

    Grid skeleton

---
+++ script.js
@@ -0,0 +1,71 @@
+ let container = document.querySelector(".data-container");
+ const template = document.querySelector("template");
+ const endpoint = "https://spreadsheets.google.com/feeds/list/14zgpcaQEX7A3CRcGnQX5rMbImw_OoG_9FypgNzZ4kh4/od6/public/values?alt=json";
+ let alleSkrifttyper = [];
+ let filter = "alle";
+ document.addEventListener("DOMContentLoaded", start);
+
+ function start() {
+     loadData();
+     addEventListenersToButtons();
+ }
+
+
+
+
+ async function loadData() {
+     const response = await fetch(endpoint);
+     alleSkrifttyper = await response.json();
+     console.log(alleSkrifttyper);
+     visSkrifttyper();
+ }
+
+
+
+ function visSkrifttyper() {
+
+     container.innerHTML = "";
+
+     alleSkrifttyper.feed.entry.forEach(skrifttype => {
+         if (filter == "alle" || filter == skrifttype.gsx$type.$t) {
+
+             let klon = template.cloneNode(true).content;
+             klon.querySelector("h3").textContent = skrifttype.gsx$navn.$t;
+             klon.querySelector("img").src = `img/${skrifttype.gsx$imgs.$t}.svg`;
+             klon.querySelector("#style").textContent = skrifttype.gsx$style.$t;
+             klon.querySelector("#classic").textContent = skrifttype.gsx$classic.$t;
+//             klon.querySelector("#overskriftbrodtekst").textContent = skrifttype.gsx$overskriftbrodtekst.$t;
+             //             klon.querySelector(".skrifttyper").addEventListener("click", () => {
+             //                 location.href = "detalje.html?id=" + skrifttype.gsx$id.$t;
+             //             });
+
+
+             container.appendChild(klon);
+
+         }
+
+
+     })
+
+
+ }
+
+ function addEventListenersToButtons() {
+     console.log("Button clicked");
+     document.querySelectorAll(".filter").forEach(elm => {
+         elm.addEventListener("click", filtrering);
+     })
+ }
+
+ function filtrering() {
+     console.log("FILTER");
+     filter = this.dataset.kategori;
+
+     document.querySelectorAll(".filter").forEach(elm => {
+         elm.classList.remove("valgt");
+     })
+
+     this.classList.add("valgt");
+     visSkrifttyper();
+     document.querySelector(".skrifttyper_kategori").textContent = this.textContent;
+ }
